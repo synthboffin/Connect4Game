c186687280e80aa011209407688c0b7fc272c403
synthboffin <47209094+synthboffin@users.noreply.github.com>
2019-02-04 16:55:32 +0000

    Added calculator quiz

--- src/index.js
+++ src/index.js
@@ -3,228 +3,126 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function Circle(props) {
-  var color = "white";
-  if (props.cell == 1) {
-    color = "black";
-  } else if (props.cell == 2) {
-    color = "red";
-  }
-  var style = {
-    backgroundColor: color,
-    border: "1px solid black",
-    borderRadius: "100%",
-    paddingTop: "98%"
-  };
-  return <div style={style} />;
-}
+const Question = props => <h1>{props.question}?</h1>;
 
-function Cell(props) {
-  var style = {
-    height: 50,
-    width: 50,
-    border: "1px solid black",
-    backgroundColor: "yellow"
-  };
+const Answer = props => {
+  const { handleAnswerClick, answer } = props;
+  return <button onClick={() => handleAnswerClick(props.id)}>{answer}</button>;
+};
 
+const Answers = props => {
+  const answers = props.answers.map((a, index) => (
+    <Answer
+      key={index}
+      id={index}
+      handleAnswerClick={props.handleClick}
+      answer={a}
+    />
+  ));
+  return <div>{answers}</div>;
+};
+
+const Counter = props => {
   return (
-    <div style={style} onClick={() => props.handleClick(props.row, props.col)}>
-      <Circle cell={props.cell} />
-    </div>
+    <h3>
+      {props.title}: {props.count}
+    </h3>
   );
-}
+};
 
-function Row(props) {
-  var style = {
-    display: "flex"
-  };
-  var cells = [];
-  for (let i = 0; i < 7; i++) {
-    cells.push(
-      <Cell
-        key={i}
-        cell={props.cells[i]}
-        row={props.row}
-        col={i}
-        handleClick={props.handleClick}
-      />
-    );
-  }
-  return <div style={style}>{cells}</div>;
-}
-
-function Board(props) {
-  var rows = [];
-  for (let i = 5; i >= 0; i--) {
-    rows.push(
-      <Row
-        key={i}
-        row={i}
-        cells={props.cells[i]}
-        handleClick={props.handleClick}
-      />
-    );
-  }
-  return <div>{rows}</div>;
-}
-
-class Game extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
 
-    var cells = [];
-    for (let i = 0; i < 6; i++) {
-      cells.push(new Array(7).fill(0));
-    }
+    const [question, answers, correct] = this.createNewQuestion();
 
-    this.state = { player: false, cells: cells, winner: 0 };
+    this.state = {
+      correctCount: 0,
+      incorrectCount: 0,
+      question: question,
+      answers: answers,
+      correct: correct
+    };
   }
 
-  restart = () => {
-    var cells = [];
-    for (let i = 0; i < 6; i++) {
-      cells.push(new Array(7).fill(0));
+  getOperatorText = operator => {
+    switch (operator) {
+      case 0:
+        return "+";
+      case 1:
+        return "-";
+      case 2:
+        return "*";
+      case 3:
+        return "/";
+      default:
+        return;
     }
-    this.setState({ player: false, cells: cells, winner: 0 });
   };
 
-  handleClick = (row, col) => {
-    if (this.state.winner) return;
+  getOperandText = operand =>
+    "" + operand > 0 ? operand : "(" + operand + ")";
 
-    console.log("row: " + row + " | col: " + col);
-    var temp = [];
-    for (let i = 0; i < 6; i++) {
-      temp.push(this.state.cells[i].slice());
+  calculateAnswer = (operand1, operand2, operator) => {
+    switch (operator) {
+      case 0:
+        return operand1 + operand2;
+      case 1:
+        return operand1 - operand2;
+      case 2:
+        return operand1 * operand2;
+      case 3:
+        return operand1 / operand2;
+      default:
+        return;
     }
-
-    var newRow = this.findAvailableRow(col);
-    temp[newRow][col] = this.state.player ? 1 : 2;
-    this.setState({ cells: temp, player: !this.state.player }, () => {
-      if (this.checkVictory(newRow, col) > 0) {
-        console.log("win");
-        this.setState({ winner: this.state.player ? 2 : 1 });
-      }
-    });
   };
 
-  findAvailableRow(col) {
-    for (var i = 0; i < 6; i++) {
-      if (this.state.cells[i][col] == 0) {
-        return i;
-      }
-    }
-    return -1;
-  }
+  createNewQuestion() {
+    const operand1 = Math.round(Math.random() * 50 - 25);
+    const operand2 = Math.round(Math.random() * 50 - 25);
+    const operator = Math.floor(Math.random() * 3);
 
-  checkDiagonal(row, col) {
-    //find right and left tops
-    var c = this.state.cells;
-    var val = this.state.player ? 2 : 1;
-    var rR = row;
-    var cR = col;
-    while (rR < 5 && cR < 6) {
-      rR++;
-      cR++;
-    }
-
-    while (rR >= 3 && cR >= 3) {
-      if (
-        c[rR][cR] == val &&
-        c[rR - 1][cR - 1] == val &&
-        c[rR - 2][cR - 2] == val &&
-        c[rR - 3][cR - 3] == val
-      ) {
-        return 1;
-      }
-      rR--;
-      cR--;
-    }
-
-    var rL = row;
-    var cL = col;
-
-    while (rL < 5 && cL > 0) {
-      rL++;
-      cL--;
-    }
-
-    while (rL >= 3 && cL <= 3) {
-      if (
-        c[rL][cL] == val &&
-        c[rL - 1][cL + 1] == val &&
-        c[rL - 2][cL + 2] == val &&
-        c[rL - 3][cL + 3] == val
-      ) {
-        return 1;
-      }
-      rL--;
-      cL++;
-    }
-    return 0;
-  }
-
-  checkHorizontal(row, col) {
-    var c = this.state.cells;
-    var i = 6;
-    var val = this.state.player ? 2 : 1;
-
-    while (i >= 3) {
-      if (
-        c[row][i] == val &&
-        c[row][i - 1] == val &&
-        c[row][i - 2] == val &&
-        c[row][i - 3] == val
-      ) {
-        return 1;
-      }
-      i--;
-    }
-    return 0;
-  }
-
-  checkVertical(row, col) {
-    var c = this.state.cells;
-    var i = row;
-    var val = this.state.player ? 2 : 1;
-
-    if (i >= 3) {
-      if (
-        c[i][col] == val &&
-        c[i - 1][col] == val &&
-        c[i - 2][col] == val &&
-        c[i - 3][col] == val
-      ) {
-        return 1;
-      }
-    }
-    return 0;
-  }
-
-  checkVictory(row, col) {
-    return (
-      this.checkVertical(row, col) ||
-      this.checkHorizontal(row, col) ||
-      this.checkDiagonal(row, col)
+    let answers = Array.from(Array(5), _ =>
+      Math.round(Math.random() * 50 - 25)
     );
+    let answerPosition = Math.floor(Math.random() * 5);
+
+    let question =
+      "Please tell me, what is " +
+      this.getOperandText(operand1) +
+      this.getOperatorText(operator) +
+      this.getOperandText(operand2);
+
+    let correct = this.calculateAnswer(operand1, operand2, operator);
+    //answers.push(correct);
+    answers[answerPosition] = correct;
+
+    return [question, answers, correct];
   }
+
+  handleAnswerClick = answerId => {
+    if (this.state.answers[answerId] === this.state.correct)
+      this.setState({ correctCount: this.state.correctCount + 1 });
+    else this.setState({ incorrectCount: this.state.incorrectCount + 1 });
+
+    const [question, answers, correct] = this.createNewQuestion();
+    this.setState({ question: question, answers: answers, correct: correct });
+  };
 
   render() {
     return (
-      <div>
-        <h1>
-          {this.state.winner > 0
-            ? this.state.winner == 1
-              ? "Black Wins"
-              : "Red Wins"
-            : this.state.player
-            ? "Black's Turn"
-            : "Red's Turn"}
-        </h1>
-        <Board cells={this.state.cells} handleClick={this.handleClick} />
-        <button onClick={() => this.restart()}>Restart</button>
+      <div className="App">
+        <Question question={this.state.question} />
+        <Answers
+          handleClick={this.handleAnswerClick}
+          answers={this.state.answers}
+        />
+        <Counter title="Correct" count={this.state.correctCount} />
+        <Counter title="Incorrect" count={this.state.incorrectCount} />
       </div>
     );
   }
 }
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
